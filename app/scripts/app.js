ade447ba5ba158b018009d2b458f91ced918ae9d
Julio Gonzalez <julio_Carol@hotmail.com>
2017-09-22 18:50:02 -0700

    Assignment 5 Controllers: complete

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -13,13 +13,14 @@
              templateUrl: '/templates/landing.html'
          })
         .state('album', {
-          url: '/album',
-          templateUrl: '/templates/album.html'
+            url: '/album',
+            controller: 'AlbumCtrl as album',
+            templateUrl: '/templates/album.html'
         })
         .state('collection', {
-          url: '/collection',
-          controller: 'CollectionCtrl as collection',
-          templateUrl: '/templates/collection.html'
+            url: '/collection',
+            controller: 'CollectionCtrl as collection',
+            templateUrl: '/templates/collection.html'
       });
     }
 
